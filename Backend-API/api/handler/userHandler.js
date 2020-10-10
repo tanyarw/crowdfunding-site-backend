e63869abe30fd3ed4f07694f44ef432bd64c86b2
Tanya Warrier <51485771+tanyarw@users.noreply.github.com>
2020-10-10 20:07:40 +0530

    EJS files

---
+++ Backend-API/api/handler/userHandler.js
@@ -0,0 +1,69 @@
+
+const jwt = require('jsonwebtoken')
+const User = require('../../Models/User');
+const { validationResult } = require('express-validator');
+
+exports.updateUser = async (req, res, next) => {
+    const userId= req.params.userId;
+    const tabuserId= req.userId;
+    if (tabuserId== userId){
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error('Validation failed, entered data is incorrect.');
+      error.statusCode = 422;
+      throw error;
+    }
+    const name= req.body.name;
+    const lastname= req.body.lastname;
+    const phone = req.body.phone;
+    const profession= req.body.profession;
+    User.findById(userId)
+    .then(user=>{
+      if(!user){
+        const error = new Error('Could not find user.');
+        error.statusCode = 404;
+        throw error;
+      }
+      user.name= name;
+      user.lastname= lastname;
+      user.phone = phone;
+      user.profession= profession;
+      user.save()
+      res.status(200).json({ message: 'User updated!', post: user});
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+}else{
+
+      res.status(422).json('User account does not match');
+      
+}
+  
+  }
+  
+  exports.getUser = async (req, res, next) => {
+    const userId= req.params.userId;
+    const tabuserId= req.userId;
+    if (tabuserId== userId){
+      User.findById(userId)
+      .then(result=>{
+        
+        res.status(200).json({ message: 'User Details Got', user: result});
+      })
+      .catch(err => {
+        if (!err.statusCode) {
+          err.statusCode = 500;
+        }
+        next(err);
+      });
+    }
+    else{
+
+      res.status(422).json('User account does not match');
+      
+    }
+  }
---
+++ Backend-API/app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//OPEN IMPORT OF THE ROUTES
+const authRoutes = require('./api/routes/authRoutes'); 
+const fundraiserRoutes = require('./api/routes/fundraiserRoutes'); 
+const userRoutes = require('./api/routes/userRoutes'); 
+const billingRoutes = require('./api/routes/billingRoutes');
+//CLOSE IMPORT OF THE ROUTES
+mongoose.Promise= global.Promise;
+
+mongoose
+     .connect( "mongodb+srv://tanya:tanya@iwp.c0ufz.mongodb.net/wildsprint?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+     .then(() => console.log( 'Database Connected' ))
+     .catch(err => console.log( err ));
+app.use(morgan('dev'));
+app.use(express.static('upload'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Header','Origin,X-Requested-with,Content-Type,Accept,Authorization');
+    
+    if(req.method=='OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, GET, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+app.set("view engine", "ejs");        
+app.use('/auth',authRoutes);
+app.use('/fundraiser',fundraiserRoutes);
+app.use('/user',userRoutes);
+app.use('/bill',billingRoutes);
+app.use((error, req,res,next)=>{
+    console.log(error);
+    const status= error.statusCode || 500;
+    const message = error.message;
+    const data= error.data;
+    res.status(status).json({message: message, data: data})
+})
+
+app.get("/login", function (req, res) { 
+    res.render("login"); 
+});
+
+app.get("/signup", function (req, res) { 
+    res.render("signup"); 
+});
+
+
+
+module.exports = app;
